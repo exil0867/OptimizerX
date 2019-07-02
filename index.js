f6052ece55efdcdcdc1382179ce5e76d6891a2e0
ExillustX <mrexillustx@gmail.com>
2019-07-02 23:35:50 +0100

    add main code

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+require('dotenv').config();
 const isRunning = require('./lib/is-running');
+const macFilter = require('./lib/mac-filter');
+let processList = process.env.PROCESS_LIST.split(',').map(p => p.trim().toLowerCase());
+let status = false;
 
-isRunning('', (status) => {
-  console.log(status);
-});
+setInterval(() => {
+  console.log('Interval started!');
+  isRunning(processList, async (running) => {
+    if (running) {
+      console.log('Detected a running process!')
+      if (!status) {
+        console.log('Mac filter is set to ON!')
+        await macFilter(true);
+        status = true;
+      }
+    } else {
+      console.log('The process is not running');
+      if (status) {
+        console.log('Mac filter is set to OFF!')
+        await macFilter(false);
+        status = false;
+      }
+    }
+  });
+}, process.env.INTERVAL)
