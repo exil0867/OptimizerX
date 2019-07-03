c55a1e506992c6f60e3a1e3653375c5892e13e6c
ExillustX <mrexillustx@gmail.com>
2019-07-03 14:30:52 +0100

    made the main function non-async, add an initial MAC-filter reset before the loop starts

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 require('dotenv').config();
 const isRunning = require('./lib/is-running');
 const macFilter = require('./lib/mac-filter');
+
 let processList = process.env.PROCESS_LIST.split(',').map(p => p.trim().toLowerCase());
 let status = false;
+macFilter(false);
 
 setInterval(() => {
   console.log('Interval started!');
-  isRunning(processList, async (running) => {
+  isRunning(processList, (running) => {
     if (running) {
       console.log('Detected a running process!')
       if (!status) {
         console.log('Mac filter is set to ON!')
-        await macFilter(true);
+        macFilter(true);
         status = true;
       }
     } else {
       console.log('The process is not running');
       if (status) {
         console.log('Mac filter is set to OFF!')
-        await macFilter(false);
+        macFilter(false);
         status = false;
       }
     }
